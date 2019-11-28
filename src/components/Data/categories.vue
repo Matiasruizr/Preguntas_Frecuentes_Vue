b0bb6d8c610c837ae98124e760c0d88648c72c37
Jose <jmmontes@uc.cl>
2019-11-28 18:03:18 -0300

    configuracion express

--- src/components/Data/categories.vue
+++ src/components/Data/categories.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="cat" >
-        <!-- <h1>{{upList}}</h1> -->
-        <h3><a v-if="lastElem == 0" @click="increment(id), update_upList(name)">{{name}}</a>
+
+        <h3><a v-if="lastElem == 0" @click="increment(id)">{{name}}</a>
         <a v-else>{{name}}</a></h3>
         <p>{{descripcion}}</p>
         <span v-if="link" ><a :href="link">{{link}}</a></span>
@@ -24,12 +24,12 @@ export default {
 
 
   methods: {
-     ...mapMutations(['increment', 'update_upList', 'indexUpList']),
+     ...mapMutations(['increment']),
 
   },
   computed: {
 
-    ...mapState(['count', 'upList']), 
+    ...mapState(['count']), 
 
   }
 }
--- src/components/Externos/upList.vue
+++ src/components/Externos/upList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="upList" @click="update_upList(name)">
-    <a v-for="element in upList" :key="element.name" >
+    <a>
     {{element.name}}
     <span v-if="upList.length > 1"> > </span>
     </a>
@@ -10,28 +10,30 @@
 
 <script>
 
-// import { mapState, mapMutations} from 'vuex'
-import store from '../../store/index.js'
+import  {mapState, mapMutations } from 'vuex'
+// import store from '../../store/index.js'
 
 
 export default {
-    store,
-    props: ['titulo'],
-    index: 0,
-    upList: [{name: 'Preguntas Frecuentes'}],
+    // store,
+    props: ['name', 'link'],
+    // index: 0,
+    // upList: [{name: 'Preguntas Frecuentes'}],
+    data () {
+        return {
 
-
-   mutations:
-   { 
-    update_upList (state, element) {
-        
-    state.upList.push({name: element}) 
-
+        }
+    
     },
-    indexUpList(state, element) {
-    state.index = state.array.name.indexOf(element)
+    methods: {
+     ...mapMutations(['increment', 'update_upList', 'indexUpList']),
 
-    }
   },
+  computed: {
+
+    ...mapState(['count', 'upList']), 
+
+  }
+
 }
 </script>
