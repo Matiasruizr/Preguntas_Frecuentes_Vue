ec2d06993f483889623c2ef88b1be25f6cde2f2f
Matias Ruiz <matias@acomeradmin.cl>
2019-11-29 15:55:33 -0300

    Fix de serve

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ var serveStatic = require('serve-static');
 
 var app = express();
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.Port || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port);
+// console.log('server started '+ port);
