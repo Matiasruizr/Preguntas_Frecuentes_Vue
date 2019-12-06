24d8c098b2797a07d5d62ebddea33481cb8dbfb2
Jose <jmmontes@uc.cl>
2019-12-06 18:10:08 -0300

    sendgrid

--- src/App.vue
+++ src/App.vue
@@ -68,7 +68,7 @@ export default {
         headers: {
           'Authorization' : 'Bearer ' + this.apikey,
           'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Origin': 'https://preguntas-frecuentes.herokuapp.com',
           'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
           'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
         },
