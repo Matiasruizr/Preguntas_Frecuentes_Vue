0261abf81bbc0dcae17d364be3e1de18c10b7af6
Jose <jmmontes@uc.cl>
2019-12-09 13:24:39 -0300

    sendgrid2

--- src/components/Services/api-gateway.js
+++ src/components/Services/api-gateway.js
@@ -7,7 +7,7 @@ var jmAWS = axios.create({
       'Authorization': 'Bearer ' + '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',
     //   'Content-Type': 'application/json',
     //   'Access-Control-Allow-Origin': 'https://preguntas-frecuentes.herokuapp.com',
-    //   'Access-Control-Allow-Methods': 'POST',
+      'Access-Control-Allow-Methods': 'POST',
     //   'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
     },
     body: {
